2b9d33534e33c48738d9612a1b40f64a27d2df4b
Alfredo <maicol.13alfrdo@hotmail.com>
2023-07-04 21:55:23 -0500

    primer commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const router = Router();
+
+router.get("/", (req, res) =>
+  res.render("index", { title: "First Web with node" })
+);
+
+router.get("/about", (req, res) => res.render("about", { title: "About Me" }));
+
+router.get("/contact", (req, res) =>
+  res.render("contact", { title: "Contact Page" })
+);
+
+export default router;
